d340e4d023fd3771154084500e1d9cb42e3d1b02
FEB2025 <sameertupe68@gmail.com>
2025-12-01 16:21:01 +0530

    Changes at 12/1/2025

---
+++ src/main/java/org/event/manage/Helper/ServiceHelper.java
@@ -0,0 +1,9 @@
+package org.event.manage.Helper;
+
+import org.event.manage.Service.EventService;
+import org.event.manage.Service.EventServiceImpl;
+
+public class ServiceHelper {
+    static EventService eventService = new EventServiceImpl();
+
+}
